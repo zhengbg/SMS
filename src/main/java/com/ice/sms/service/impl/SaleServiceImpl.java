0bc65655a2dd7795b40b0a4bf5c8dbb32a656ada
huangsongbo <huangsongbo@quanten.cn>
2017-12-06 18:41:05 +0800

    销售记录 操作 dao层实现

---
+++ src/main/java/com/ice/sms/service/impl/SaleServiceImpl.java
@@ -0,0 +1,81 @@
+/**
+ * 文 件 名:  SaleServiceImpl
+ * 版    权:  Quanten Teams. Copyright YYYY-YYYY,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  huangsongbo
+ * 修改时间:  2017/12/6
+ * 跟踪单号:  <跟踪单号>
+ * 修改单号:  <修改单号>
+ * 修改内容:  <修改内容>
+ */
+package com.ice.sms.service.impl;
+
+import com.ice.sms.common.base.ResultInfo;
+import com.ice.sms.dao.SaleDao;
+import com.ice.sms.dto.sale.request.BatchDelSaleReq;
+import com.ice.sms.dto.sale.request.QuerySaleByIdReq;
+import com.ice.sms.dto.sale.request.QuerySaleReq;
+import com.ice.sms.dto.sale.response.QuerySaleByIdResp;
+import com.ice.sms.dto.sale.response.QuerySaleResp;
+import com.ice.sms.entity.SaleDo;
+import com.ice.sms.service.SaleService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * <一句话功能简述> <功能详细描述>
+ *
+ * @author huangsongbo
+ * @version 2017/12/6
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+@Service
+public class SaleServiceImpl implements SaleService
+{
+	private static final Logger LOGGER = LogManager.getLogger (SaleServiceImpl.class);
+	@Autowired
+	private SaleDao saleDao;
+
+	@Override
+	public QuerySaleResp querySaleRecord (QuerySaleReq req)
+	{
+		return null;
+	}
+
+	@Override
+	public QuerySaleByIdResp querySaleRecordById (QuerySaleByIdReq req)
+	{
+		return null;
+	}
+
+	@Override
+	public ResultInfo addSale (SaleDo saleDo)
+	{
+		LOGGER.debug (String.format ("SMS.SaleServiceImpl.addSale.request:%s", saleDo));
+		ResultInfo resp = new ResultInfo ();
+
+
+		return resp;
+	}
+
+	@Override
+	public ResultInfo updateSale (SaleDo saleDo)
+	{
+		LOGGER.debug (String.format ("SMS.SaleServiceImpl.updateSale.request:%s", saleDo));
+		ResultInfo resp = new ResultInfo ();
+
+		return resp;
+	}
+
+	@Override
+	public ResultInfo batchDelSale (BatchDelSaleReq req)
+	{
+		LOGGER.debug (String.format ("SMS.SaleServiceImpl.batchDelSale.request:%s", req));
+		ResultInfo resp = new ResultInfo ();
+
+		return resp;
+	}
+}
