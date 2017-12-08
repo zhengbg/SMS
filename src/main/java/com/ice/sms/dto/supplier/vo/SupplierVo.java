3db2a486b4f62862d2dbbfe1cfd2d9eee6018bfb
zbg <2423142354@qq.com>
2017-12-08 17:32:03 +0800

    ss

--- src/main/java/com/ice/sms/dto/supplier/vo/SupplierVo.java
+++ src/main/java/com/ice/sms/dto/supplier/vo/SupplierVo.java
@@ -37,24 +37,4 @@ public class SupplierVo implements Serializable
 	private String phone;
 	private String email;
 	private String memo;
-
-	public static void main(String args[]) {
-
-		Thread t = new Thread() {
-
-			public void run() {
-				pong();
-			}
-		};
-
-		t.start ();
-		System.out.print("ping");
-
-	}
-
-	static void pong() {
-
-		System.out.print("pong");
-
-	}
 }
